a02fc7af03e405e434cbda31d3b0533670454515
jritter77 <jr550@humboldt.edu>
2021-08-12 13:43:40 -0700

    - Adjusted images in about page to look better on mobile.

--- client/views/about.js
+++ client/views/about.js
@@ -3,11 +3,11 @@ function About() {
     
     app.innerHTML = `
     <div class="row" style='margin-top:5vw;'>
-        <div class="col">
-            <img src=./images/aboutPic1.jpg style='width:100%;'>
-            <img src=./images/aboutPic2.jpg style='width:100%;margin-top:2vw;'>
+        <div class="col-md">
+            <img class='img-fluid' src=./images/aboutPic1.jpg >
+            <img class='img-fluid' src=./images/aboutPic2.jpg style='margin-top:2vw;'>
         </div>
-        <div class="col-6">
+        <div class="col-md-6">
             <p>Humboldt Crystals is located in Eureka along the Pacific coast nestled among the majestic redwood trees. My business evolved over time.</p> 
             <p>I began working with beautiful beads and sparkling crystals making suncatchers as a way to relieve stress and bring positive creative energy into my life when I was caring for my mother who suffered with Alzheimer's.</p>
             <p>As time went on I realized I needed to find homes for the many bright, cheerful suncatchers I was making. So I plunged into world of local events and festivals. Its been great fun.</p>
@@ -15,9 +15,9 @@ function About() {
             <p>I enjoy the challenge of finding new and unique shaped crystals to incorporate into my one of a kind designs. I hand build all my pieces, with carefully chosen glass beads, quality crystals and often incorporate Pacific Coast driftwood, abalone shells or handsome metal pendants. I am committed to creating quality suncatchers, car charms and earrings along with great customer service.</p>
             <p>I take great pride in each of my creations and get pleasure from seeing the joy they bring to others.</p>
         </div>
-        <div class="col">
-            <img src=./images/aboutPic3.jpg style='width:100%;'>
-            <img src=./images/aboutPic4.jpg style='width:100%;margin-top:2vw;'>
+        <div class="col-md">
+            <img class='img-fluid' src=./images/aboutPic3.jpg >
+            <img class='img-fluid' src=./images/aboutPic4.jpg style='margin-top:2vw;'>
         </div>
     </div>
     `;
