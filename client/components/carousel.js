55a0ecc32f99e9a3b2ab891d949fd9b60d8c6bd0
jritter77 <jr550@humboldt.edu>
2021-09-14 14:58:26 -0700

    - modified carousel to fit better for mobile and desktop

--- client/components/carousel.js
+++ client/components/carousel.js
@@ -3,19 +3,23 @@
 function Carousel(articles) {
 
     const imgWidth = '100%';
-    const imgHeight = '30vw';
+    const imgHeight = (window.innerWidth > 768) ? '30vw' : '40vw';
 
     function fillImages() {
         let html = "";
 
         let slides = 1;
 
+        // limits slides if not enough articles present
         if (articles.length >= 6) {
             slides = 2;
         }
         else if (articles.length >= 9) {
             slides = 3;
         }
+
+        console.log(document.documentElement.clientWidth, window.innerWidth);
+
 
         for (let slide=0; slide<slides; slide++) {
             
@@ -25,7 +29,7 @@ function Carousel(articles) {
                 <div class='row no-gutters'>
                     <div class='col'><img style="width:${imgWidth};height:${imgHeight};" src=${articles[articles.length-(1+3*slide)].img}></div>
                     <div class='col'><img style="width:${imgWidth};height:${imgHeight};" src=${articles[articles.length-(2+3*slide)].img}></div>
-                    <div class='col'><img style="width:${imgWidth};height:${imgHeight};" src=${articles[articles.length-(3+3*slide)].img}></div>
+                    ${(window.innerWidth > 768) ? `<div class='col'><img style="width:${imgWidth};height:${imgHeight};" src=${articles[articles.length-(3+3*slide)].img}></div>` : ''}
                 </div>    
             </div>`
 
